4c5a5548b6e57d49391f594f2fbd2e696ed733d5
Karan Jayakumar <karanjayakumar22@gmail.com>
2026-05-10 15:03:33 +0530

    resolved bugss

--- transform.py
+++ transform.py
@@ -4,7 +4,6 @@ import random
 import yaml
 
 from torchvision.transforms import v2
-from dataset import ChangeDetctionDataset
 
 with open("config.yaml", "r") as f:
     cfg = yaml.safe_load(f)
@@ -34,7 +33,7 @@ class PairedTransform:
         self.sar_augment = v2.RandomApply(
             [
                 v2.Lambda(lambda x: x *torch.empty(1).uniform_(0.98,1.02)),
-            ], p=aug["color_jitter"]["probability"])
+            ], p=aug["intensity_scale"]["probability"])
         
         self.blur = v2.RandomApply(
             [
@@ -85,3 +84,29 @@ class PairedTransform:
             mask = v2.functional.vflip(mask)
         
         return image1, image2, mask
+
+
+class ValTransform:
+    def __init__(self):
+        pass
+    def __call__(self, image1, image2, mask):
+        image2 = self._sar_normalization(image2)
+        image1 = self._optical_normalization(image1)
+
+        image1 = torch.from_numpy(image1).float().permute(2, 0, 1)
+        image2 = torch.from_numpy(image2).float().permute(2, 0, 1)
+        mask = torch.from_numpy(mask.squeeze()).long()
+
+        return {"image1": image1, "image2": image2, "mask": mask}
+
+    def _sar_normalization(self, image2):
+        p2, p98 = np.percentile(image2, (2, 98))
+        image2 = np.clip(image2, p2, p98)
+        image2 = (image2 - image2.min())/(image2.max() - image2.min() + 1e-6)
+        return image2
+        
+    def _optical_normalization(self, image1):
+        p2, p98 = np.percentile(image1, (2, 98))
+        image1 = np.clip(image1, p2, p98)
+        image1 = (image1 - image1.min())/(image1.max() - image1.min() + 1e-6)
+        return image1
